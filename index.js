2e4ab73c581cc8b6da62e7a00d320378a2baabef
Razvanc1006 <razvanc1006@gmail.com>
2021-06-09 20:02:08 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,98 @@
+<<<<<<< HEAD
+const todoContainer = document.getElementById('todo-container');
+const form = document.querySelector('form');
+
+form.addEventListener('submit',function(e){
+    e.preventDefault();
+
+    const description= form.querySelector('input').value;
+    const newTodo = {description};
+
+    fetch('http://localhost:3000/api/todos', {
+            method: "POST",
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem('token')}`,
+                'Content-Type': 'application/json'
+            },
+            body:JSON.stringify(newTodo),
+        })
+        .then(response => response.json())
+        .then(()=> getTodos());
+    
+    
+});
+
+function displayTodos(todos){
+    todoContainer.innerHTML= '';
+    for(let todo of todos)
+    {
+        const li = document.createElement('li');
+        li.textContent= todo.description;
+        li.classList = todo.done ? "done" : '';
+        todoContainer.appendChild(li);
+        const span = document.createElement('span');
+        span.textContent = 'X';
+        li.appendChild(span);
+        
+        
+        registerDoneEvent(li,todo);
+        registerDeleteEvent(li,todo);
+    }
+   
+}
+
+function registerDoneEvent(li,todo){
+    li.addEventListener('click',function(){
+        //patch
+        //url: http://localhost:3000/api/todos/:todoID
+        //raspuns : noul todo -> facem update la lista -> getTodos
+
+        fetch(`http://localhost:3000/api/todos/${todo.id}`,{
+            method: 'PATCH',
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem('token')}`,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                description: todo.description,
+                done: !todo.done
+            })
+        })
+        .then(response => response.json())
+        .then(() => getTodos());
+    });
+}
+function registerDeleteEvent(li,todo){
+    const deleteButton = li.querySelector('span');
+    deleteButton.addEventListener('click',function(e){ 
+        e.stopPropagation();
+        fetch(`http://localhost:3000/api/todos/${todo.id}`,{
+            method: 'DELETE',
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem('token')}`,
+                'Content-Type': 'application/json'
+            },
+           /* body: JSON.stringify({
+                todoID: todo.id
+            }) */
+        })
+        .then(response => response.json())
+        .then(()=> li.remove());
+    });
+
+}
+
+function getTodos(){
+    fetch('http://localhost:3000/api/todos', {
+        headers: {
+            Authorization: `Bearer ${localStorage.getItem('token')}`
+        }
+    })
+    .then(response => response.json())
+    .then(todos => displayTodos(todos));
+}
+
+=======
 const todoContainer = document.getElementById('todo-container');
 const form = document.querySelector('form');
 
@@ -43,4 +138,5 @@ function getTodos(){
     .then(todos => displayTodos(todos));
 }
 
+>>>>>>> 0b5315e1fe8db4bc720d58a28df0d3683120c2d8
 getTodos();
--- login.js
+++ login.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const form= document.getElementById("login-form");
 
 let token = '';
@@ -25,4 +26,33 @@ form.addEventListener('submit',function(e){
     })
 
     
+=======
+const form= document.getElementById("login-form");
+
+let token = '';
+
+
+
+form.addEventListener('submit',function(e){
+    e.preventDefault();
+    
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value ;
+    const user = {email,password};
+    
+    // http://localhost:3000/api/todos
+    fetch('http://localhost:3000/api/login',{
+        method: 'POST',
+        body: user
+    })
+    .then(response => response.json())
+    .then(response => {
+        localStorage.setItem('token',response.token);
+        //redirect to index.html
+        window.location.href= '/';
+        
+    })
+
+    
+>>>>>>> 0b5315e1fe8db4bc720d58a28df0d3683120c2d8
 });
